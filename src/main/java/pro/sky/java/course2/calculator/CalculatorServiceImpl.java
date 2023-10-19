14618d13d16326387d6f17ccf61afbc5030bd57c
SkynetBuilder <vlad.gulenkov@gmail.com>
2023-10-19 20:56:26 +0300

    Задание выполнено (исправил опечатку)

--- src/main/java/pro/sky/java/course2/calculator/CalculatorServiceImpl.java
+++ src/main/java/pro/sky/java/course2/calculator/CalculatorServiceImpl.java
@@ -16,7 +16,7 @@ public class CalculatorServiceImpl implements CalculatorService {
 
     @Override
     public int multiply(int num1, int num2) {
-        return num1 + num2;
+        return num1 * num2;
     }
 
     @Override
